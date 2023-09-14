b16d642f70a06e65e1fe25440b251ce05ca8dbf1
driftlog <driftlogged@gmail.com>
2023-09-15 05:56:12 +0800

    help

--- miniproject-backend/src/main/java/sg/edu/nus/iss/miniproject/configs/SecurityConfig.java
+++ miniproject-backend/src/main/java/sg/edu/nus/iss/miniproject/configs/SecurityConfig.java
@@ -1,36 +1,36 @@
-// package sg.edu.nus.iss.miniproject.configs;
+package sg.edu.nus.iss.miniproject.configs;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-// import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-// import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-// @RequiredArgsConstructor
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-//     @Autowired
-//     private UserAuthProvider userAuthProvider;
+    @Autowired
+    private UserAuthProvider userAuthProvider;
 
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http.addFilterBefore(new JwtAuthFilter(userAuthProvider), BasicAuthenticationFilter.class)
-//             .csrf(AbstractHttpConfigurer::disable)
-//             .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//             .authorizeHttpRequests((requests) -> requests
-//                 .requestMatchers(HttpMethod.POST, "/","/login", "/api/login", "/api/register", "/user/sendMail", "/user/postEvent/").permitAll()
-//                 .requestMatchers(HttpMethod.GET, "/assets/", "/", "/", "/user/getEvent/", "user/getEvents/").permitAll()
-//                 .requestMatchers(HttpMethod.DELETE, "/user/deleteEvent").permitAll()
-//                 .anyRequest().authenticated());
-//         return http.build();
-//     }
-// }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new JwtAuthFilter(userAuthProvider), BasicAuthenticationFilter.class)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers(HttpMethod.POST, "/","/login", "/api/login", "/api/register", "/user/sendMail", "/user/postEvent/").permitAll()
+                .requestMatchers(HttpMethod.GET, "/assets/", "/", "/", "/user/getEvent/", "user/getEvents/").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/user/deleteEvent").permitAll()
+                .anyRequest().authenticated());
+        return http.build();
+    }
+}
